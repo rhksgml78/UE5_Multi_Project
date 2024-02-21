0ca7a905f14fa61a35f8736ad60a1edc2fa3f773
신관 <rhksgml78@naver.com>
2024-02-21 12:54:40 +0900

    24.02.21_NewProject
    새로운 프로젝트 생성

---
+++ SKH_MultiShooting/Source/SKH_MultiShooting/SKH_MultiShootingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SKH_MultiShootingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKH_MULTISHOOTING_API ASKH_MultiShootingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
