5b640e6f91ea202f0f4e9649b1c5eec32d671435
신관 <rhksgml78@naver.com>
2024-02-21 12:58:41 +0900

    Update .gitignore

--- SKH_MultiShooting/Source/SKH_MultiShooting/SKH_MultiShooting.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SKH_MultiShooting.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SKH_MultiShooting, "SKH_MultiShooting" );
