7e124529faf4bc58edce72393727795461b6dabf
신관 <rhksgml78@naver.com>
2024-02-23 17:20:39 +0900

    24.02.23_맵추가(BigStarStation)

---
+++ SKH_MultiShooting/Source/SKH_MultiShooting/GameMode/LobbyGameMode.cpp
@@ -0,0 +1,16 @@
+#include "LobbyGameMode.h"
+#include "GameFramework/GameStateBase.h"
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	// 게임에 참가한 인원 확인 가능
+	int32 NumberOfPlayers = GameState.Get()->PlayerArray.Num();
+
+	if (NumberOfPlayers == 2)
+	{
+
+	}
+
+}
---
+++ SKH_MultiShooting/Source/SKH_MultiShooting/GameMode/LobbyGameMode.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+
+#include "LobbyGameMode.generated.h"
+
+
+UCLASS()
+class SKH_MULTISHOOTING_API ALobbyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+
+};
