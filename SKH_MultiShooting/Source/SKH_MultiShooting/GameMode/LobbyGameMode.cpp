d2c2208c04cb6be662a179ef1443753a66a1b15c
신관 <rhksgml78@naver.com>
2024-02-25 00:47:09 +0900

    24.02.25_Config폴더내용수정

--- SKH_MultiShooting/Source/SKH_MultiShooting/GameMode/LobbyGameMode.cpp
+++ SKH_MultiShooting/Source/SKH_MultiShooting/GameMode/LobbyGameMode.cpp
@@ -8,7 +8,7 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 	// 게임에 참가한 인원 확인 가능
 	int32 NumberOfPlayers = GameState.Get()->PlayerArray.Num();
 
-	if (NumberOfPlayers == 2)
+	if (NumberOfPlayers == 1)
 	{
 		UWorld* World = GetWorld();
 		if (World)
