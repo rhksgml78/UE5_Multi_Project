545de738dd060a23b976672fd23f46959483314d
신관 <rhksgml78@naver.com>
2024-03-05 22:08:13 +0900

    24.03.05_탄피사운드와타이머

--- SKH_MultiShooting/Source/SKH_MultiShooting/Weapon/Casing.cpp
+++ SKH_MultiShooting/Source/SKH_MultiShooting/Weapon/Casing.cpp
@@ -1,5 +1,7 @@
 #include "Casing.h"
 #include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
 
 ACasing::ACasing()
 {
@@ -7,13 +9,60 @@ ACasing::ACasing()
 
 	CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CasingMesh"));
 	SetRootComponent(CasingMesh);
+	CasingMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	CasingMesh->SetSimulatePhysics(true);
+	CasingMesh->SetEnableGravity(true);
+	CasingMesh->SetNotifyRigidBodyCollision(true);
 
+	// 탄피가 배출되는 힘
+	EjectionImpulse = 5.f;
+
+	// 탄피가 소멸디는 시간
+	DestroyTimer = 3.f;
+
+	// 사운드는 한번만 재생 하기 위한 변수
+	SoundPlayOnce = false;
 }
 
 void ACasing::BeginPlay()
 {
 	Super::BeginPlay();
 
+	// 스태틱메시의 Hit와 바인딩
+	CasingMesh->OnComponentHit.AddDynamic(this, &ThisClass::OnHit);
+	CasingMesh->AddImpulse(GetActorForwardVector() * EjectionImpulse);
 
+
+	// 생성되자마자 5초의 타이머를 실행
+	if (this)
+	{
+		// 타이머 설정
+		FTimerHandle TimerHandle;
+		FTimerDelegate TimerDel;
+
+		// 델리게이트에 Destroy 함수 바인딩
+		TimerDel.BindLambda([this]()
+			{
+				this->Destroy();
+			});
+
+		// 5초 후에 Destroy 함수 실행
+		GetWorld()->GetTimerManager().SetTimer(TimerHandle, TimerDel, DestroyTimer, false);
+	}
+
+}
+
+void ACasing::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	// 충돌시 파괴 후 충돌 사운드 재생
+
+	if (DropSound && !SoundPlayOnce)
+	{
+		SoundPlayOnce = true;
+		UGameplayStatics::PlaySoundAtLocation(this, DropSound, GetActorLocation());
+	}
+
+	//Destroy();
+	
 }
 
--- SKH_MultiShooting/Source/SKH_MultiShooting/Weapon/Casing.h
+++ SKH_MultiShooting/Source/SKH_MultiShooting/Weapon/Casing.h
@@ -5,6 +5,11 @@
 
 #include "Casing.generated.h"
 
+/*
+해당클래스는 복제생성X
+본인이 발사한 탄피만 보이도록 플레이에 본인만 보인다.
+*/
+
 UCLASS()
 class SKH_MULTISHOOTING_API ACasing : public AActor
 {
@@ -13,11 +18,27 @@ class SKH_MULTISHOOTING_API ACasing : public AActor
 public:	
 	ACasing();
 
+protected:
+	virtual void BeginPlay() override;
+
+	// 피격이벤트(바인딩해야함) 바닥이나 어딘가 충돌했을때 객체가 파괴 되도록
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
 private:
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* CasingMesh;
 
-protected:
-	virtual void BeginPlay() override;
+	UPROPERTY(EditAnywhere)
+	float EjectionImpulse;
+
+	UPROPERTY(EditAnywhere)
+	float DestroyTimer;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* DropSound;
+
+	bool SoundPlayOnce;
+
 
 };
