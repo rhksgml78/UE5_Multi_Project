45237c4a18412ac10da45e2fb777d629026b86d1
신관 <rhksgml78@naver.com>
2024-03-23 16:09:19 +0900

    24.03.23_권총파티클생성
    서버 클라 모두 문제 없음

--- SKH_MultiShooting/Source/SKH_MultiShooting/Weapon/HitScanWeapon.cpp
+++ SKH_MultiShooting/Source/SKH_MultiShooting/Weapon/HitScanWeapon.cpp
@@ -2,6 +2,7 @@
 #include "Engine/SkeletalMeshSocket.h"
 #include "SKH_MultiShooting/Character/PlayerCharacter.h"
 #include "Kismet/GameplayStatics.h"
+#include "particles/ParticleSystemComponent.h"
 
 void AHitScanWeapon::Fire(const FVector& HitTarget)
 {
@@ -12,11 +13,13 @@ void AHitScanWeapon::Fire(const FVector& HitTarget)
 	{
 		return;
 	}
+	
+	// 모든 피격계산은 서버에서하기때문에 크라이언트에서 InstigatorController 는 항상 null이다.
 	AController* InstigatorController = OwnerPawn->GetController();
 
 	const USkeletalMeshSocket* MuzzleFlashSocket = GetWeaponMesh()->GetSocketByName("MuzzleFlash");
 
-	if (MuzzleFlashSocket && InstigatorController)
+	if (MuzzleFlashSocket)
 	{
 		FTransform SocketTransform = MuzzleFlashSocket->GetSocketTransform(GetWeaponMesh());
 
@@ -33,23 +36,28 @@ void AHitScanWeapon::Fire(const FVector& HitTarget)
 				End,
 				ECollisionChannel::ECC_Visibility
 			);
+
+			// 파티클의 끝점을 라인트레이스의 끝으로 잡고
+			FVector BeamEnd = End;
+
 			if (FireHit.bBlockingHit)
 			{
+				// 만일 임팩트 포인트가 있다면 그곳으로 다시 저장
+				BeamEnd = FireHit.ImpactPoint;
 				APlayerCharacter* PlayerCharacter = Cast<APlayerCharacter>(FireHit.GetActor());
-				if (PlayerCharacter)
+
+				if (PlayerCharacter && HasAuthority() && InstigatorController)
 				{
-					if (HasAuthority())
-					{
-						// 서버에서만 데미지 계산
-						UGameplayStatics::ApplyDamage(
-							PlayerCharacter,
-							Damage,
-							InstigatorController,
-							this,
-							UDamageType::StaticClass()
-						);
-					}
+					// 컨트롤러는 서버에서만 값을 가지고있으며 클라이언트에서는 null을 반환한다 따라서 해당 조건문은 플레이어 캐릭터 && 서버 && 서버 의 조건문으로 무조건 서버에서만 하는 작업이 된다. 데미지 계산은 서버에서만 한다.
+					UGameplayStatics::ApplyDamage(
+						PlayerCharacter,
+						Damage,
+						InstigatorController,
+						this,
+						UDamageType::StaticClass()
+					);
 				}
+
 				if (ImpactParticles)
 				{
 					UGameplayStatics::SpawnEmitterAtLocation(
@@ -58,6 +66,21 @@ void AHitScanWeapon::Fire(const FVector& HitTarget)
 						FireHit.ImpactPoint,
 						FireHit.ImpactNormal.Rotation()
 					);
+				}
+			}
+			// 위의 작업종료후 일직선의 빔파티클 생성
+			if (BeamParticles)
+			{
+				UParticleSystemComponent* Beam = UGameplayStatics::SpawnEmitterAtLocation(
+					World,
+					BeamParticles,
+					SocketTransform
+				);
+				if (Beam)
+				{
+					// 컴포넌트를통해 생성한 이펙트의 파라미터중 Target 이란느 파라미터의 값을 BeamEnd로 지정한다.
+
+					Beam->SetVectorParameter(FName("Target"), BeamEnd);
 				}
 			}
 		}
--- SKH_MultiShooting/Source/SKH_MultiShooting/Weapon/HitScanWeapon.h
+++ SKH_MultiShooting/Source/SKH_MultiShooting/Weapon/HitScanWeapon.h
@@ -23,4 +23,7 @@ private:
 	UPROPERTY(EditAnywhere)
 	class UParticleSystem* ImpactParticles;
 
+	UPROPERTY(EditAnywhere)
+	UParticleSystem* BeamParticles;
+
 };
